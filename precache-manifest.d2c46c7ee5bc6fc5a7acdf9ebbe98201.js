e415ca2b37325dc9864228cba892e4603bff93e8
zainbinfurqan <zain.ahmed199524@gmail.com>
2020-09-06 15:27:16 +0500

    Updates

---
+++ precache-manifest.d2c46c7ee5bc6fc5a7acdf9ebbe98201.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7fa812c2aef48b488d5194d8963e56a",
+    "url": "/gh-pages-development/index.html"
+  },
+  {
+    "revision": "fc1448516c971b30f532",
+    "url": "/gh-pages-development/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "5736c21e6ec81c09cea9",
+    "url": "/gh-pages-development/static/js/2.ca50d39c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-development/static/js/2.ca50d39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc1448516c971b30f532",
+    "url": "/gh-pages-development/static/js/main.d806cc0e.chunk.js"
+  },
+  {
+    "revision": "699e0235058446b19f10",
+    "url": "/gh-pages-development/static/js/runtime-main.72a98dbb.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/gh-pages-development/static/media/logo.ee7cd8ed.svg"
+  }
+]);

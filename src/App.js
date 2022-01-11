b4b4d4c51a88a992300c98d3445b74f7d7ce58b9
Zain Ahmed <zain.ahmed@backcountry.com>
2022-01-11 17:07:34 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import './App.css';
-import { Provider } from 'react-redux'
-import store from './redux'
+import React from "react";
+import "./App.css";
+// import { Provider } from "react-redux";
+// import store from "./redux";
 // import ToDoApp from './ToDoApp'
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <p>Hellow all github actions</p>
-        {/* <ToDoApp /> */}
-      </div>
-    </Provider>
+    // <Provider store={store}>
+    <div className="App">
+      <p>Hellow all github actions</p>
+      {/* <ToDoApp /> */}
+    </div>
+    // </Provider>
   );
 }
 

72d3048244b392c34aaa648f0fca33fa91e437a1
Zain Ahmed <zain.ahmed@backcountry.com>
2022-01-11 16:46:35 +0500

    commit

---
+++ build/precache-manifest.a9d97dbf16d1b4ad554aadb43877e75a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a437b0c428bb59083363e0ed011149b",
+    "url": "/zainbinfurqan-github-pages-deployment/index.html"
+  },
+  {
+    "revision": "48aae2012c24caffdc75",
+    "url": "/zainbinfurqan-github-pages-deployment/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b7d79fa1bde23aae3310",
+    "url": "/zainbinfurqan-github-pages-deployment/static/js/2.7d051ba6.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/zainbinfurqan-github-pages-deployment/static/js/2.7d051ba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48aae2012c24caffdc75",
+    "url": "/zainbinfurqan-github-pages-deployment/static/js/main.993e3691.chunk.js"
+  },
+  {
+    "revision": "7188f2e86514b24fb32d",
+    "url": "/zainbinfurqan-github-pages-deployment/static/js/runtime-main.302de773.js"
+  }
+]);
